b1b6ffdf6c796c30a798a31c087eab35b37e6ded
xkk1 <68276846+xkk1@users.noreply.github.com>
2021-09-21 17:18:54 +0800

    Update 找色差作弊.user.js

--- program/Tampermonkey/找色差作弊.user.js
+++ program/Tampermonkey/找色差作弊.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         找色差作弊
-// @namespace    https://xkk1.github.io/?Tempermonley=找色差作弊
-// @version      0.0.5
-// @description  try to take over the world!
+// @namespace    https://xkk1.github.io/program/Tampermonkey/#找色差作弊
+// @version      0.0.7
+// @description  https://www.zhaosecha.com/这个找色差游戏的作弊脚本，妈妈再也不怕我分不清颜色了
 // @author       小喾苦
 // @match        https://www.zhaosecha.com/
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @updateURL https://xkk1.github.io/program/Tampermonkey/%E6%89%BE%E8%89%B2%E5%B7%AE%E4%BD%9C%E5%BC%8A.user.js
 // ==/UserScript==
 
 (function() {
