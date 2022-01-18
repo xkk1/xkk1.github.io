f4e79043443bb43b3f5ded242c287e60f6c07e0c
xkk1 <68276846+xkk1@users.noreply.github.com>
2022-01-18 16:56:32 +0800

    Add files via upload

---
+++ program/Tampermonkey/4399弹窗.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         4399弹窗
+// @namespace    https://xkk1.github.io/program/Tampermonkey/#4399弹窗
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       小喾苦
+// @match        http://www.4399.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    //document.getElementsByClassName("fcmdialog")[0].style.display = "none"
+    //document.getElementsByClassName("fcmdialog")[0].style.display = "cmMask"
+    function a(){Anti_close();}
+    setTimeout(a, 1000)
+
+})();
