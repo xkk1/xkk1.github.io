a899dd906508b53855752f35c6a4fe2aa95a2a94
小喾苦 <68276846+xkk1@users.noreply.github.com>
2023-09-06 17:14:24 +0800

    更新 真谷歌生草机 0.2.0

--- program/Tampermonkey/真谷歌生草机.user.js
+++ program/Tampermonkey/真谷歌生草机.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         真谷歌生草机
 // @namespace    https://xkk1.github.io/program/Tampermonkey/#真谷歌生草机
-// @version      0.1.0
+// @version      0.2.0
 // @description  更改谷歌翻译网页上的文字 https://translate.google.com/
 // @author       小喾苦
+// @match        https://translate.google.com/
 // @match        https://translate.google.com/?*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
@@ -14,24 +15,22 @@
 
     // Your code here...
     var 标题 = "谷歌生草机";
-    var 左上翻译改为 = "生草机";
-    var 主菜单中翻译改为 = "生草机";
-    var 主菜单中关于_Google_翻译改为 = "关于 Google 生草机";
+    var 翻译改为 = "生草机";
+    var 关于_Google_翻译改为 = "关于 Google 生草机";
 
     // main
     document.title = 标题;
-    try {
-        /*
-        var gb_5d_gb_Vc = document.getElementsByClassName("gb_fd gb_6c"); // 它竟然变了，原来是“gb_5d gb_Vc” “gb_4d gb_1c”
-        gb_5d_gb_Vc[0].textContent = 主菜单中翻译改为;
-        gb_5d_gb_Vc[1].textContent = 左上翻译改为;
-        */
-        var gb_yc_a_span = document.querySelectorAll(".gb_yc>a>span");
-        gb_yc_a_span[1].textContent = 主菜单中翻译改为;
-        gb_yc_a_span[3].textContent = 左上翻译改为;
-        document.getElementsByClassName("yJWPX")[0].innerText =主菜单中关于_Google_翻译改为; //'关于 Google 翻译'
-    } catch (e){
-        console.log(e);
-        alert("来自油猴脚本：真谷歌生草机\n谷歌翻译已更新，本脚本失效!\n请联系原作者发布新版脚本\n哔哩哔哩：小喾苦   UID513689605\nQQ：3434623263");
+    let all_span = document.getElementsByTagName("span");
+
+    for(let i = 0; i < all_span.length; i++){
+        if(all_span[i].innerHTML == "翻译"){
+            all_span[i].innerHTML = 翻译改为;
+        }
+    }
+    let all_a = document.getElementsByTagName("a");
+    for(let i = 0; i < all_a.length; i++){
+        if(all_a[i].innerHTML == "关于 Google 翻译"){
+            all_a[i].innerHTML = 关于_Google_翻译改为;
+        }
     }
 })();
